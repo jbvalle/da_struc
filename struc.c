be1ead4250d5990c1fa3051688ae1a82a91113d0
John Bryan Valle <jbvalle.jv@gmail.com>
2021-06-30 16:06:42 +0200

    added arrays of structs

--- struc.c
+++ struc.c
@@ -11,6 +11,8 @@ int main(int argc, char const *argv[]) {
   struct Rectangle r1;
   struct Rectangle r2 = {3, 4};
 
-  printf("%d", r2.width);
+  struct Rectangle r3[2] = {{12,15},{8,6}};
+  printf("%d\n", r2.width);
+  printf("%d\n", r3[0].length );
   return 0;
 }
--- struc.cpp
+++ struc.cpp
@@ -13,6 +13,11 @@ int main(int argc, char const *argv[]) {
   struct Rectangle r1;
   struct Rectangle r2 = {3, 4};
 
-  cout << r2.width;
+
+  struct Rectangle r3[2] = {{12,15},{8,6}};
+
+
+  cout << r2.width << endl;
+  cout << r3[1].width;
   return 0;
 }
